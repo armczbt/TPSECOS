442277ec7e2495106ae8b20764e0cb1ebb691002
armczbt <104758831+armczbt@users.noreply.github.com>
2024-10-23 07:47:18 +0000

    TP2

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,16 +1,46 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <intr.h>
 
 void bp_handler() {
-   // TODO
+	asm volatile ("pusha");
+	uint32_t val;
+   	asm volatile ("mov 4(%%ebp), %0":"=r"(val));
+	debug("%x\n",val);
+
+   	debug("WOW BP BP\n");
+	asm volatile ("pusha");
+	asm volatile ("leave");
+	asm volatile ("iret");
 }
 
 void bp_trigger() {
-	// TODO
+	
+	asm volatile ("int3");
 }
 
 void tp() {
 	// TODO print idtr
+	unsigned int IDT_add;
+	get_idtr(IDT_add);
+	debug("IDT Add 0x%x\n",IDT_add);
+
+	idt_reg_t idt_ptr;
+	get_idtr(idt_ptr);
+	debug("IDT Add 0x%x\n",(unsigned int)&IDT_add);
+
+	unsigned int add_BP_Handler;
+	add_BP_Handler = (unsigned int)&bp_handler;
+	debug("BPHandler Add 0x%x\n",add_BP_Handler);
+
+	idt_ptr.desc[3].offset_1 = (unsigned int)&bp_handler & 0xFFFF;
+	idt_ptr.desc[3].offset_2 = ((unsigned int)&bp_handler)>>16 & 0xFFFF;
+
+	bp_trigger();
+
+
+	debug("Bien revenu\n");
+
 
 	// TODO call bp_trigger
    //bp_trigger();
